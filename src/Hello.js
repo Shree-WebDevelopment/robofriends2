1174a2a52757c2522321025113750aa58bae2871
Shree-WebDevelopment <ashreekrishna7@gmail.com>
2021-07-01 00:26:42 +1000

    my robofriends2 pushing for portfolio upgrade

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Hello extends Component {
+	render(){
+		return (
+		<div>
+		<h1> helllooooo </h1>
+		<p> welcome to react </p>
+
+	</div>
+	);
+ }
+
+}
+export default Hello;
